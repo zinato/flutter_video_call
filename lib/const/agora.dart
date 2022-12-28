eaf4d5f71aac39466431918dc33d55b528f95739
Paul <hyunil@braincolla.com>
2022-12-28 17:25:12 +0900

    create: flutter 영상통화 앱 코드 구현

---
+++ lib/const/agora.dart
@@ -0,0 +1,5 @@
+const APP_ID = ''; //Insert Your AgoraAppID
+
+const CHANNEL_NAME = ''; //Insert Your AgoraAppID
+
+const TEMP_TOKEN = ''; //Insert your Agora Temp_token
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_video_call/screen/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      fontFamily: 'NotoSans',
+    ),
+    home: HomeScreen(),
+  ));
+}
+
